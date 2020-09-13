6e92ba3f75149fb4a569bb67a8fe3b5471b68fe1
Todor Diklich <todordiklich@gmail.com>
2020-09-13 21:39:39 +0300

    Trees - Lab
    Implementation of trees - BFS & DFS

---
+++ 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Tree.cs
@@ -0,0 +1,50 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private readonly List<Tree<T>> _children;
+
+        public Tree(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Value { get; private set; }
+        public Tree<T> Parent { get; private set; }
+        public IReadOnlyCollection<Tree<T>> Children => this._children.AsReadOnly();
+
+        public ICollection<T> OrderBfs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<T> OrderDfs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
