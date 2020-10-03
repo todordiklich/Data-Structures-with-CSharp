d56442ae7965b851917b5a56e0bdf8854007d0a7
Todor Diklich <todordiklich@gmail.com>
2020-10-03 12:57:35 +0300

    Exam solution

---
+++ Fundamentals/Exam/01.Inventory/Inventory.cs
@@ -0,0 +1,190 @@
+﻿namespace _01.Inventory
+{
+    using _01.Inventory.Interfaces;
+    using _01.Inventory.Models;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    public class Inventory : IHolder
+    {
+        private List<IWeapon> _elements;
+
+        public Inventory()
+        {
+            this._elements = new List<IWeapon>();
+        }
+
+        public int Capacity => this._elements.Count;
+
+        public void Add(IWeapon weapon)
+        {
+            this._elements.Add(weapon);
+        }
+
+        public void Clear()
+        {
+            this._elements.Clear();
+        }
+
+        public bool Contains(IWeapon weapon)
+        {
+            int elementIndex = this.GetIndexById(weapon.Id);
+
+            if (elementIndex == -1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void EmptyArsenal(Category category)
+        {
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                IWeapon current = this._elements[i];
+
+                if (current.Category == category)
+                {
+                    current.Ammunition = 0;
+                }
+            }
+        }
+
+        public bool Fire(IWeapon weapon, int ammunition)
+        {
+            int index = this.GetIndexById(weapon.Id);
+            this.EnsureIndexIsCorrect(index);
+
+            if (this._elements[index].Ammunition - ammunition >= 0)
+            {
+                this._elements[index].Ammunition -= ammunition;
+                return true;
+            }
+
+            return false;
+        }
+
+        public IWeapon GetById(int id)
+        {
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                IWeapon current = this._elements[i];
+
+                if (current.Id == id)
+                {
+                    return current;
+                }
+            }
+
+            return null;
+        }
+
+        public int Refill(IWeapon weapon, int ammunition)
+        {
+            int index = this.GetIndexById(weapon.Id);
+            this.EnsureIndexIsCorrect(index);
+
+            if (this._elements[index].Ammunition + ammunition >= this._elements[index].MaxCapacity)
+            {
+                this._elements[index].Ammunition = this._elements[index].MaxCapacity;
+            }
+            else
+            {
+                this._elements[index].Ammunition += ammunition;
+            }
+
+            return this._elements[index].Ammunition;
+        }
+
+        public IWeapon RemoveById(int id)
+        {
+            int index = this.GetIndexById(id);
+            this.EnsureIndexIsCorrect(index);
+            IWeapon weapon = this._elements[index];
+            this._elements.RemoveAt(index);
+
+            return weapon;
+        }
+
+        public int RemoveHeavy()
+        {
+            int count = this._elements.RemoveAll(e => e.Category == Category.Heavy);
+
+            return count;
+        }
+
+        public List<IWeapon> RetrieveAll()
+        {
+            List<IWeapon> result = new List<IWeapon>(this._elements);
+
+            return result;
+        }
+
+        public List<IWeapon> RetriveInRange(Category lower, Category upper)
+        {
+            List<IWeapon> result = new List<IWeapon>();
+
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                if (this._elements[i].Category >= lower && this._elements[i].Category <= upper)
+                {
+                    result.Add(this._elements[i]);
+                }
+            }
+
+            return result;
+        }
+
+        public void Swap(IWeapon firstWeapon, IWeapon secondWeapon)
+        {
+            int firstWeaponIndex = this.GetIndexById(firstWeapon.Id);
+            int secondWeaponIndex = this.GetIndexById(secondWeapon.Id);
+
+            this.EnsureIndexIsCorrect(firstWeaponIndex);
+            this.EnsureIndexIsCorrect(secondWeaponIndex);
+
+            if (firstWeapon.Category == secondWeapon.Category)
+            {
+                this._elements[firstWeaponIndex] = secondWeapon;
+                this._elements[secondWeaponIndex] = firstWeapon;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this._elements.GetEnumerator();
+        }
+
+        private void EnsureWeaponExist(IWeapon weapon)
+        {
+            if (!this.Contains(weapon))
+            {
+                throw new InvalidOperationException("Weapon does not exist in inventory!");
+            }
+        }
+
+        private int GetIndexById(int id)
+        {
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                if (this._elements[i].Id == id)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void EnsureIndexIsCorrect(int id)
+        {
+            if (id < 0 || id >= this.Capacity)
+            {
+                throw new InvalidOperationException("Weapon does not exist in inventory!");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Exam/02.LegionSystem/Legion.cs
@@ -0,0 +1,93 @@
+﻿namespace _02.LegionSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using _02.LegionSystem.Interfaces;
+    using Wintellect.PowerCollections;
+
+    public class Legion : IArmy
+    {
+        private OrderedSet<IEnemy> _elements;
+
+        public Legion()
+        {
+            this._elements = new OrderedSet<IEnemy>();
+        }
+        public int Size => this._elements.Count;
+
+        public bool Contains(IEnemy enemy)
+        {
+            return this._elements.Contains(enemy);
+        }
+
+        public void Create(IEnemy enemy)
+        {
+            this._elements.Add(enemy);
+        }
+
+        public IEnemy GetByAttackSpeed(int speed)
+        {
+            if (this._elements.GetFirst().AttackSpeed > speed || this._elements.GetLast().AttackSpeed < speed)
+            {
+                return null;
+            }
+
+            return this._elements.First(e => e.AttackSpeed == speed);
+        }
+
+        public List<IEnemy> GetFaster(int speed)
+        {
+            List<IEnemy> result = this._elements.FindAll(e => e.AttackSpeed > speed).ToList();
+
+            return result;
+        }
+
+        public IEnemy GetFastest()
+        {
+            this.EnsureNotEmpty();
+
+            return this._elements.GetLast();
+        }
+
+        public IEnemy[] GetOrderedByHealth()
+        {
+            //TODO: create new ordered set with Healt
+            return this._elements.OrderByDescending(e => e.Health).ToArray();
+        }
+
+        public List<IEnemy> GetSlower(int speed)
+        {
+            List<IEnemy> result = this._elements.FindAll(e => e.AttackSpeed < speed).ToList();
+
+            return result;
+        }
+
+        public IEnemy GetSlowest()
+        {
+            this.EnsureNotEmpty();
+
+            return this._elements.GetFirst();
+        }
+
+        public void ShootFastest()
+        {
+            this.EnsureNotEmpty();
+            this._elements.RemoveLast();
+        }
+
+        public void ShootSlowest()
+        {
+            this.EnsureNotEmpty();
+            this._elements.RemoveFirst();
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("Legion has no enemies!");
+            }
+        }
+    }
+}
