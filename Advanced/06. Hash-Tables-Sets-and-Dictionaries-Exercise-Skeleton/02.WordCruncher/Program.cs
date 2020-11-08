33c2605c9841c829a7bf9823525b22162ab9d561
Todor Diklich <todordiklich@gmail.com>
2020-11-08 23:15:16 +0200

    Adding filter to the result

--- Advanced/06. Hash-Tables-Sets-and-Dictionaries-Exercise-Skeleton/02.WordCruncher/Program.cs
+++ Advanced/06. Hash-Tables-Sets-and-Dictionaries-Exercise-Skeleton/02.WordCruncher/Program.cs
@@ -10,28 +10,56 @@ namespace _02.WordCruncher
         public static int attempt = 0;
         static void Main(string[] args)
         {
+            //a aa aaa aaaa
+            //aaaa
             List<string> input = Console.ReadLine()
-                .Split(new char[] {' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
-                .Distinct()
-                .OrderBy(s => s)
-                .ToList();
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            //.Split(", ")
+            .Distinct()
+            .OrderBy(s => s)
+            .ToList();
 
             string target = Console.ReadLine();
 
             FindMatch(input, target);
+            FilterResult();
 
             foreach (var item in dict.Values)
             {
-                if (item.Count > 0)
+                if (item.Count > 0 && target == item.Aggregate((i, j) => i + j))
                 {
                     Console.WriteLine(string.Join(" ", item));
                 }
             }
         }
 
+        private static void FilterResult()
+        {
+            Dictionary<int, List<string>> filteredDict = new Dictionary<int, List<string>>();
+            int index = 0;
+            foreach (var item in dict.Values)
+            {
+                filteredDict[index] = item.OrderBy(s => s).ToList();
+                index++;
+            }
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < dict.Keys.Count - 1; i++)
+            {
+                if (filteredDict[i].SequenceEqual(filteredDict[i+1]))
+                {
+                    indexes.Add(i+1);
+                }
+            }
+            indexes.Reverse();
+            for (int i = 0; i < indexes.Count; i++)
+            {
+                dict.Remove(indexes[i]);
+            }
+        }
+
         private static void FindMatch(List<string> input, string target)
         {
-            if (target == "")
+            if (string.IsNullOrEmpty(target))
             {
                 attempt += 1;
                 var result = new List<string>(dict[attempt - 1]);
