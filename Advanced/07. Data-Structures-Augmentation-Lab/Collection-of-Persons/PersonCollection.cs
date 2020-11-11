8e178bd7c5534be8b31130312c282f40105da896
todordiklich <todordiklich@gmail.com>
2020-11-11 13:56:08 +0200

    changes

--- Advanced/07. Data-Structures-Augmentation-Lab/Collection-of-Persons/PersonCollection.cs
+++ Advanced/07. Data-Structures-Augmentation-Lab/Collection-of-Persons/PersonCollection.cs
@@ -2,45 +2,132 @@
 {
     using System;
     using System.Collections.Generic;
+    using Wintellect.PowerCollections;
 
     public class PersonCollection : IPersonCollection
     {
-        // TODO: define the underlying data structures here ...
+        private Dictionary<string, Person> personByEmail = new Dictionary<string, Person>();
+
+        private Dictionary<string, SortedSet<Person>> personByDomain = 
+            new Dictionary<string, SortedSet<Person>>();
+
+        private Dictionary<string, SortedSet<Person>> personByNameAndTown =
+            new Dictionary<string, SortedSet<Person>>();
+
+        private OrderedDictionary<int, SortedSet<Person>> personByAge =
+            new OrderedDictionary<int, SortedSet<Person>>();
+
+        private Dictionary<string, OrderedDictionary<int, SortedSet<Person>>> personByTownAge =
+            new Dictionary<string, OrderedDictionary<int, SortedSet<Person>>>();
 
         public bool AddPerson(string email, string name, int age, string town)
         {
-            throw new NotImplementedException();
+            if (this.FindPerson(email) != null)
+            {
+                return false;
+            }
+
+            Person person = new Person() { Email = email, Name = name, Age = age, Town = town };
+            this.personByEmail.Add(email, person);
+
+            var emailDomain = this.ExtractDomain(email);
+            this.personByDomain.AppendValueToKey(emailDomain, person);
+
+            var nameAndTown = this.CombineNameAndTown(name, town);
+            this.personByNameAndTown.AppendValueToKey(nameAndTown, person);
+
+            this.personByAge.AppendValueToKey(age, person);
+
+            this.personByTownAge.EnsureKeyExists(town);
+            this.personByTownAge[town].AppendValueToKey(age, person);
+
+            return true;
         }
 
-        public int Count { get; }
+        public int Count { get => this.personByEmail.Count; }
         public Person FindPerson(string email)
         {
-            throw new NotImplementedException();
+            Person person = null;
+            bool ersonExist = this.personByEmail.TryGetValue(email, out person);
+
+            return person;
         }
 
         public bool DeletePerson(string email)
         {
-            throw new NotImplementedException();
+            var person = this.FindPerson(email);
+            if (person == null)
+            {
+                return false;
+            }
+
+            var personDeleted = this.personByEmail.Remove(email);
+
+            var emailDomain = this.ExtractDomain(email);
+            this.personByDomain[emailDomain].Remove(person);
+
+            var nameAndTown = this.CombineNameAndTown(person.Name, person.Town);
+            this.personByNameAndTown[nameAndTown].Remove(person);
+
+            this.personByAge[person.Age].Remove(person);
+
+            this.personByTownAge[person.Town][person.Age].Remove(person);
+
+            return true;
         }
 
         public IEnumerable<Person> FindPersons(string emailDomain)
         {
-            throw new NotImplementedException();
+            return this.personByDomain.GetValuesForKey(emailDomain);
         }
 
         public IEnumerable<Person> FindPersons(string name, string town)
         {
-            throw new NotImplementedException();
+            var nameAndTown = this.CombineNameAndTown(name, town);
+            return this.personByNameAndTown.GetValuesForKey(nameAndTown);
         }
 
         public IEnumerable<Person> FindPersons(int startAge, int endAge)
         {
-            throw new NotImplementedException();
+            var personInRange = this.personByAge.Range(startAge, true, endAge, true);
+
+            foreach (var personByAge in personInRange)
+            {
+                foreach (var person in personByAge.Value)
+                {
+                    yield return person;
+                }
+            }
         }
 
         public IEnumerable<Person> FindPersons(int startAge, int endAge, string town)
         {
-            throw new NotImplementedException();
+            if (!this.personByTownAge.ContainsKey(town))
+            {
+                yield break;
+            }
+
+            var personInRange = this.personByTownAge[town].Range(startAge, true, endAge, true);
+
+            foreach (var personByTown in personInRange)
+            {
+                foreach (var person in personByTown.Value)
+                {
+                    yield return person;
+                }
+            }
+        }
+
+        private string ExtractDomain(string email)
+        {
+            string domain = email.Split('@')[1];
+            return domain;
+        }
+
+        private string CombineNameAndTown(string name, string town)
+        {
+            const string separator = "|!|";
+            return name + separator + town;
         }
     }
 }
--- Advanced/07. Data-Structures-Augmentation-Lab/Collection-of-Persons/PersonCollectionSlow.cs
+++ Advanced/07. Data-Structures-Augmentation-Lab/Collection-of-Persons/PersonCollectionSlow.cs
@@ -2,45 +2,100 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class PersonCollectionSlow : IPersonCollection
     {
-        // TODO: define the underlying data structures here ...
+        List<Person> people = new List<Person>();
 
         public bool AddPerson(string email, string name, int age, string town)
         {
-            throw new NotImplementedException();
+            int index = GetPersonIndex(email);
+            if (index == -1)
+            {
+                Person person = new Person()
+                {
+                    Email = email,
+                    Name = name,
+                    Age = age,
+                    Town = town
+                };
+
+                people.Add(person);
+                return true;
+            }
+
+            return false;
         }
 
-        public int Count { get; }
+        public int Count => people.Count;
         public Person FindPerson(string email)
         {
-            throw new NotImplementedException();
+            int index = GetPersonIndex(email);
+
+            if (index == -1)
+            {
+                return null;
+            }
+
+            return people[index];
         }
 
         public bool DeletePerson(string email)
         {
-            throw new NotImplementedException();
+            int index = GetPersonIndex(email);
+            if (index == -1)
+            {
+                return false;
+            }
+
+            people.RemoveAt(index);
+            return true;
         }
 
         public IEnumerable<Person> FindPersons(string emailDomain)
         {
-            throw new NotImplementedException();
+            return people
+                .Where(p => p.Email.Split('@')[1] == emailDomain)
+                .OrderBy(p => p.Email);
         }
 
         public IEnumerable<Person> FindPersons(string name, string town)
         {
-            throw new NotImplementedException();
+            return people
+                .Where(p => p.Name == name && p.Town == town)
+                .OrderBy(p => p.Email);
         }
 
         public IEnumerable<Person> FindPersons(int startAge, int endAge)
         {
-            throw new NotImplementedException();
+            return people
+                .Where(p => p.Age >= startAge && p.Age <= endAge)
+                .OrderBy(p => p.Age)
+                .ThenBy(p => p.Email);
         }
 
         public IEnumerable<Person> FindPersons(int startAge, int endAge, string town)
         {
-            throw new NotImplementedException();
+            return people
+                .Where(p => p.Age >= startAge && p.Age <= endAge && p.Town == town)
+                .OrderBy(p => p.Age)
+                .ThenBy(p => p.Email);
+        }
+
+        private int GetPersonIndex(string email)
+        {
+            int index = -1;
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (people[i].Email == email)
+                {
+                    index = i;
+                    return index;
+                }
+            }
+
+            return index;
         }
     }
 }
