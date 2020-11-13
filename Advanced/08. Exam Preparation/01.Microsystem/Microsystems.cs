18d350f84d49dbf4223bf562ab9f5fe0990ea434
todordiklich <todordiklich@gmail.com>
2020-11-13 14:18:01 +0200

    Exam Preparation

---
+++ Advanced/08. Exam Preparation/01.Microsystem/Microsystems.cs
@@ -0,0 +1,158 @@
+﻿namespace _01.Microsystem
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Microsystems : IMicrosystem
+    {
+        private List<Computer> computers = new List<Computer>();
+        private Dictionary<int, Computer> byNumber = new Dictionary<int, Computer>();
+        private Dictionary<string, List<Computer>> byBrand = new Dictionary<string, List<Computer>>();
+        private Dictionary<string, List<Computer>> byColor = new Dictionary<string, List<Computer>>();
+
+        public void CreateComputer(Computer computer)
+        {
+            CheckIfNumberExist(computer.Number);
+
+            //Add in computers
+            computers.Add(computer);
+
+            //Add in byNumber
+            byNumber[computer.Number] = computer;
+
+            //Add in byBrand
+            if (!byBrand.ContainsKey(computer.Brand.ToString()))
+            {
+                byBrand[computer.Brand.ToString()] = new List<Computer>();
+            }
+            byBrand[computer.Brand.ToString()].Add(computer);
+
+            //Add in byColor
+            if (!byColor.ContainsKey(computer.Color.ToString()))
+            {
+                byColor[computer.Color.ToString()] = new List<Computer>();
+            }
+            byColor[computer.Color.ToString()].Add(computer);
+        }
+
+        public bool Contains(int number)
+        {
+            return byNumber.ContainsKey(number);
+        }
+
+        public int Count()
+        {
+            return computers.Count;
+        }
+
+        public Computer GetComputer(int number)
+        {
+            CheckIfNumberDontExist(number);
+
+            return byNumber[number];
+        }
+
+        public void Remove(int number)
+        {
+            CheckIfNumberDontExist(number);
+
+            Computer computerToRemove = byNumber[number];
+
+            //Remove in computers
+            computers.Remove(computerToRemove);
+
+            //Remove in byNumber
+            byNumber.Remove(number);
+
+            //Remove in byBrand
+            byBrand[computerToRemove.Brand.ToString()].Remove(computerToRemove);
+            if (byBrand[computerToRemove.Brand.ToString()].Count == 0)
+            {
+                byBrand.Remove(computerToRemove.Brand.ToString());
+            }
+
+            //Remove in byColor
+            byColor[computerToRemove.Color.ToString()].Remove(computerToRemove);
+            if (byColor[computerToRemove.Color.ToString()].Count == 0)
+            {
+                byColor.Remove(computerToRemove.Color.ToString());
+            }
+        }
+
+        public void RemoveWithBrand(Brand brand)
+        {
+            if (!byBrand.ContainsKey(brand.ToString()))
+            {
+                throw new ArgumentException();
+            }
+
+            List<Computer> computersToremove = new List<Computer>(byBrand[brand.ToString()]);
+
+            //It can be optimized
+            foreach (var computer in computersToremove)
+            {
+                Remove(computer.Number);
+            }
+        }
+
+        public void UpgradeRam(int ram, int number)
+        {
+            CheckIfNumberDontExist(number);
+
+            Computer computer = byNumber[number];
+            if (computer.RAM < ram)
+            {
+                computer.RAM = ram;
+            }
+        }
+
+        public IEnumerable<Computer> GetAllFromBrand(Brand brand)
+        {
+            if (!byBrand.ContainsKey(brand.ToString()))
+            {
+                return new List<Computer>();
+            }
+
+            return byBrand[brand.ToString()].OrderByDescending(c => c.Price);
+        }
+
+        public IEnumerable<Computer> GetAllWithScreenSize(double screenSize)
+        {
+            return computers.Where(c => c.ScreenSize == screenSize).OrderByDescending(c => c.Number);
+        }
+
+        public IEnumerable<Computer> GetAllWithColor(string color)
+        {
+
+            if (!byColor.ContainsKey(color))
+            {
+                return new List<Computer>();
+            }
+
+            return byColor[color].OrderByDescending(c => c.Price);
+        }
+
+        public IEnumerable<Computer> GetInRangePrice(double minPrice, double maxPrice)
+        {
+            return computers.Where(c => minPrice <= c.Price && c.Price <= maxPrice)
+                .OrderByDescending(c => c.Price);
+        }
+
+        private void CheckIfNumberExist(int number)
+        {
+            if (byNumber.ContainsKey(number))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private void CheckIfNumberDontExist(int number)
+        {
+            if (!byNumber.ContainsKey(number))
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ Advanced/08. Exam Preparation/02.VaniPlanning/Agency.cs
@@ -0,0 +1,126 @@
+﻿namespace _02.VaniPlanning
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Agency : IAgency
+    {
+        private Dictionary<string, Invoice> invoices = new Dictionary<string, Invoice>();
+
+        public void Create(Invoice invoice)
+        {
+            if (invoices.ContainsKey(invoice.SerialNumber))
+            {
+                throw new ArgumentException();
+            }
+
+            invoices[invoice.SerialNumber] = invoice;
+        }
+
+        public void ThrowInvoice(string number)
+        {
+            if (!invoices.ContainsKey(number))
+            {
+                throw new ArgumentException();
+            }
+
+            invoices.Remove(number);
+        }
+
+        public void ThrowPayed()
+        {
+            var toRemove = invoices.Values.Where(i => i.Subtotal == 0)
+                .Select(i => i.SerialNumber).ToList();
+
+            foreach (var number in toRemove)
+            {
+                invoices.Remove(number);
+            }
+        }
+
+        public int Count()
+        {
+            return invoices.Count;
+        }
+
+        public bool Contains(string number)
+        {
+            return invoices.ContainsKey(number);
+        }
+
+        public void PayInvoice(DateTime due)
+        {
+            var result = invoices.Values.Where(i => i.DueDate == due).Select(i => i.Subtotal = 0);
+
+            if (result.Count() == 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public IEnumerable<Invoice> GetAllInvoiceInPeriod(DateTime start, DateTime end)
+        {
+            return invoices.Values.Where(i => start <= i.IssueDate && i.IssueDate <= end)
+                .OrderBy(i => i.IssueDate)
+                .ThenBy(i => i.DueDate);
+        }
+
+        public IEnumerable<Invoice> SearchBySerialNumber(string serialNumber)
+        {
+            var keys = invoices.Keys.Where(k => k.Contains(serialNumber));
+
+            if (keys.Count() == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return keys.OrderByDescending(k => k).Select(k => invoices[k]);
+        }
+
+        public IEnumerable<Invoice> ThrowInvoiceInPeriod(DateTime start, DateTime end)
+        {
+            var result = invoices.Values.Where(i => start < i.DueDate && i.DueDate < end);
+
+            if (result.Count() == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            foreach (var invoice in result)
+            {
+                invoices.Remove(invoice.SerialNumber);
+            }
+
+            return invoices.Values;
+        }
+
+        public IEnumerable<Invoice> GetAllFromDepartment(Department department)
+        {
+            return invoices.Values.Where(i => i.Department == department)
+                .OrderByDescending(i => i.Subtotal)
+                .ThenBy(i => i.IssueDate);
+        }
+
+        public IEnumerable<Invoice> GetAllByCompany(string company)
+        {
+            return invoices.Values.Where(i => i.CompanyName == company)
+                .OrderByDescending(i => i.SerialNumber);
+        }
+
+        public void ExtendDeadline(DateTime dueDate, int days)
+        {
+            var result = invoices.Values.Where(i => i.DueDate == dueDate);
+
+            if (result.Count() == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            foreach (var invoice in result)
+            {
+                invoice.DueDate = invoice.DueDate.AddDays(days);
+            }
+        }
+    }
+}
